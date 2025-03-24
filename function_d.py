a5dad110bb77f3a784b7e1426dfa5ea0946e01b7
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-03-24 14:25:36 -0700

    Aigerim's change

--- function_d.py
+++ function_d.py
@@ -2,8 +2,7 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
-
+    print("Aigerim's comment")
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))

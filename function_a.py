1eb512723ebe390672a9e440bee42eacba1a62bf
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-03-24 14:42:40 -0700

    Added comments

--- function_a.py
+++ function_a.py
@@ -15,7 +15,7 @@ def most_common_value(number_list):
             most_common_value = num
 
     return most_common_value
-
+#just comment 
 
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
